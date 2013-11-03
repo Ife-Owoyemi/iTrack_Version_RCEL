ff9ed3b75e9ae854baffe23945da8eed5936e105
Ife-Owoyemi <cyberife7@gmail.com>
2013-11-02 23:33:14 -0500

    shell of the RCEL version of iTrack

---
+++ iTrack_version_RCEL/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ITrackVersionRcel::Application.config.secret_token = 'aa8e1a925ca9bea6dce4fa9de7a79bd673eadd979cd9a8e9befb7477bb7fe623a4a8a741ed7a5b746f7e94aeed5d7821f80d9681400d58edbf1ce4b323af31ff'
---
+++ iTrack_version_RCEL/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ITrackVersionRcel::Application.config.session_store :cookie_store, key: '_iTrack_version_RCEL_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ITrackVersionRcel::Application.config.session_store :active_record_store
